b17b156535e7f116daa6266a5e52df2f2e6bd648
@hexlet/cli <support@hexlet.io>
2022-12-24 22:17:04 +0300

    submit java-spring-ru/relations

--- java-spring-ru/relations/src/main/java/exercise/controller/ArticlesController.java
+++ java-spring-ru/relations/src/main/java/exercise/controller/ArticlesController.java
@@ -5,7 +5,14 @@ import exercise.model.Article;
 import exercise.repository.ArticleRepository;
 
 import exercise.repository.CategoryRepository;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
